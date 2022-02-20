1dda64373823fe14ee978101fb603362a4f02eb7
vitek2711 <viktor2711@mail.ru>
2022-02-20 22:04:53 +0300

    Добавлено решение 1 задачи

---
+++ main.js
@@ -0,0 +1,24 @@
+///  МОДУЛЬ 3. ТЕМА: ВЕТВЛЕНИЕ И ФУНКЦИИ
+
+///// ЗАДАЧА -1 ////
+/*'use strict'
+
+let userYear = prompt(`Пожалуйста, введите год`);
+let wysokosny;
+let no = false;
+
+/// Пишем условия ///
+
+if (userYear % 400 == 0)
+    wysokosny = true;
+else if (userYear % 4 == 0)
+    wysokosny = true;
+else if (userYear % 100 == 0)
+    wysokosny = false;
+else
+    wysokosny = false;
+
+/// Результат выводим в консоль ///
+
+alert(`${userYear} год ${(wysokosny? '' : 'не')} является високосным`);*/
+
