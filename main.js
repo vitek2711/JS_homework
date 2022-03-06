030e4be40383539ed4e269a635fe95261308f446
vitek2711 <viktor2711@mail.ru>
2022-03-07 01:28:55 +0300

    Добавлено ЗАДАНИЕ-2

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 'use strict'
 /// ЗАДАНИЕ 1 ///
+
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // сосдаем массив нужных чисел
 
 // Пишем цикл //
-for (let num = 0; num <= arr.length - 1; num++) {
+/*for (let num = 0; num <= arr.length - 1; num++) {
 // Пишем уловия цикла
     if (num === 0) {
         console.log(`${num} - это нуль`);
@@ -16,7 +17,19 @@ for (let num = 0; num <= arr.length - 1; num++) {
     else {
         console.log(`${num} - это четное число`);
     }
+}*/
+
+/// ЗАДАНИЕ 2 ///
+
+var string = ""
+console.log(string);
+for(var x = 0; x < 20; x++) {
+    console.log(string += "X");
 }
+
+
+
+
 
 
 
