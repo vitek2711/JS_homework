126779a57f6f08e24550731e40e4e9ae8923557e
vitek2711 <viktor2711@mail.ru>
2022-04-28 23:52:44 +0300

    changed

--- calculator/main.js
+++ calculator/main.js
@@ -1,28 +1,41 @@
 'user strict'
-
+// Переменные
 let num1 = '';
 let num2 = '';
 let operSymbol = '';
 let finish = false;
 
-const numStrArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
-
-
-
-
 const buttons = document.querySelector('.buttons');
 const outPut = document.querySelector('#outPut');
 
+// Массивы с цифрами и операциями
+const numStrArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+const action  = ['-', '+', 'x', '/'];
 
+//Функция очистки значений
+function clear() {
+    a = '';
+    b= '';
+    operSymbol = '';
+    finish = false;
+    outPut.p = 0;
+}
+
+document.querySelector('#ac').onclck = clear;
+
+// Получаем событие по нажатию
 buttons.addEventListener('click', (e)=>{
     let elem = e.target;
     while (!elem.classList.contains('btn')) {
         elem = elem.parentNode;
     }
-    if (elem.dataset.num==='1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9' ){
-         outPut.insertAdjacentHTML(elem.dataset.num);
-    }
     console.log(elem.dataset.num);
+    const key = e.target.textContent;
+
+    // Проверяем нажата ли кнопка
+    if (numStrArr.includes(key)) {
+       num1 += key;
+    }
 });
 
 
